e8db3babc94936c2e7170c970801fddd8ef18685
Benjamin Janssens <benji.janssens@gmail.com>
2026-08-08 15:48:02 +0200

    refactor: move more config to .chezmoidata

--- home/.chezmoiscripts/run_after_10-homebrew.sh
+++ home/.chezmoiscripts/run_after_10-homebrew.sh
@@ -4,5 +4,5 @@ set -eu
 brew=/opt/homebrew/bin/brew
 
 "$brew" update
-"$brew" bundle install --upgrade --force-cleanup --zap --file "$HOME/.Brewfile"
+"$brew" bundle install --upgrade --force-cleanup --zap --global
 /opt/homebrew/bin/code --update-extensions
--- home/.chezmoiscripts/run_after_40-touch-id-sudo.sh
+++ home/.chezmoiscripts/run_after_40-touch-id-sudo.sh
@@ -2,21 +2,13 @@
 set -eu
 
 target=/etc/pam.d/sudo_local
-template=/etc/pam.d/sudo_local.template
 rule='auth       sufficient     pam_tid.so'
 
-if [ -r "$target" ] && /usr/bin/grep -Eq '^[[:space:]]*auth[[:space:]]+sufficient[[:space:]]+pam_tid\.so([[:space:]]|$)' "$target"; then
-  exit 0
+tmp=$(mktemp -t sudo_local)
+trap 'rm -f "$tmp"' EXIT
+printf '%s\n' "$rule" > "$tmp"
+
+if ! cmp -s "$tmp" "$target" 2>/dev/null; then
+  # root:wheel 0444 matches the stock /etc/pam.d files.
+  sudo install -o root -g wheel -m 0444 "$tmp" "$target"
 fi
-
-tmp=$(/usr/bin/mktemp "${TMPDIR:-/tmp}/sudo_local.XXXXXX")
-trap '/bin/rm -f "$tmp"' EXIT HUP INT TERM
-
-if [ -r "$target" ]; then
-  /bin/cat "$target" > "$tmp"
-elif [ -r "$template" ]; then
-  /usr/bin/sed '/^[[:space:]]*#auth[[:space:]][[:space:]]*sufficient[[:space:]][[:space:]]*pam_tid\.so[[:space:]]*$/d' "$template" > "$tmp"
-fi
-
-printf '%s\n' "$rule" >> "$tmp"
-/usr/bin/sudo /usr/bin/install -o root -g wheel -m 0444 "$tmp" "$target"
